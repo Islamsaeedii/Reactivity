20b3798bfda4e0e9706287a7f7699e7256e279b9
Islam Saeed Khodary <98616151+Islamsaeedii@users.noreply.github.com>
2022-09-29 09:41:00 +0200

    End of section 20

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -13,6 +13,7 @@ namespace Application.Core
     {
         public MappingProfiles()
         {
+            string currentUsername = null;
             CreateMap<Activity, Activity>();
             CreateMap<Activity, ActivityDto>()
             .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees
@@ -23,12 +24,19 @@ namespace Application.Core
                 .ForMember(d => d.DisplayName, o => o.MapFrom(a => a.AppUser.DisplayName))
                 .ForMember(d => d.Username, o => o.MapFrom(a => a.AppUser.UserName))
                 .ForMember(d => d.Bio, o => o.MapFrom(a => a.AppUser.Bio))
-                .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url));
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(d => d.FollowersCount, o => o.MapFrom(x => x.AppUser.Followers.Count))
+                .ForMember(d => d.FollowingCount, o => o.MapFrom(x => x.AppUser.Followings.Count))
+                .ForMember(d => d.Following, o => o.MapFrom(s => s.AppUser.Followers.Any(x => x.Observer.UserName == currentUsername)));
 
 
 
             CreateMap<AppUser, Profiles.Profile>()
-                .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url));
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain).Url))
+                .ForMember(d => d.FollowersCount, o => o.MapFrom(x => x.Followers.Count))
+                .ForMember(d => d.FollowingCount, o => o.MapFrom(x => x.Followings.Count))
+                .ForMember(d => d.Following, o => o.MapFrom(s => s.Followers.Any(x => x.Observer.UserName == currentUsername)));
+
 
             CreateMap<Comment, CommentDto>()
                 .ForMember(d => d.DisplayName, o => o.MapFrom(a => a.Author.DisplayName))
